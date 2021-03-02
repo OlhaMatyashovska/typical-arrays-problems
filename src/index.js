b65b9d93483fd774771b3952dfba56110e718e26
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-03-02 15:26:59 +0200

    feat: implement min, max and average functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array && array.length) {
+    return Math.min(...array);
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array && array.length) {
+    return Math.max(...array);
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array !== undefined  && array.length) {
+     let average = (array.reduce((accumulator,currentValue) => accumulator + currentValue))/array.length;
+     return average;
+  }
+  else {
+    return 0;
+  }
 }
